97c66de563456ba2a8d52790afffbc7fa7f7ab01
nicopiko <nstall22@student.aau.dk>
2023-02-28 15:40:32 +0100

    Testing branch switching in git

--- ColorRecognition/ColorRecognition.py
+++ ColorRecognition/ColorRecognition.py
@@ -1,24 +1,38 @@
 import cv2
 from subprocess import call
 import time as t
+import os
 
 cap = cv2.VideoCapture(index = 0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+lolarr = [" ", ".", "..", "..."]
 
 threshArray = [180, 240]
 key = None
 glove = False
 bluetime = 0
 
-x = input("Do you want to calibrate? If not, default values are used (y/n)")
-threshArray[0] = int(input("What is your lower threshold?"))
-threshArray[1] = int(input("What is your upper threshold?"))
-
 #Defining functions
 def open_file_py():
             call(["python", "If_Blue.py"])
 
+def calibrate():
+    os.system("clear")
+    for i in range(10):
+        for i in range(4):
+            print("Calibrating" + lolarr[i])
+            t.sleep(0.5)
+            os.system("clear")
+    
+    
+#Run program
+caliPrompt = input("Do you want to calibrate? If not, default values are used (y/n)")
+
+if(caliPrompt == "y"):
+    calibrate()
+    
 while(key != 27):
     _, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #Converting RGB values to HSV values
