204e5fbaab763e22bb622754320e90b0f36d1882
nicopiko <nstall22@student.aau.dk>
2023-02-28 12:39:42 +0100

    Cleaned up ColorCalibration.py

--- ColorRecognition/ColorRecognition.py
+++ ColorRecognition/ColorRecognition.py
@@ -45,21 +45,13 @@ while(key != 27):
         if bluetime > 100:
             giveItem()
             bluetime = 0
-            
-        
-        open_file_py()
-        
-        
+            open_file_py()
     else:
         glove = False
         bluetime = 0
         
     def open_file_py():
             call(["python", "If Blue.py"])    
-        
-    def giveItem():
-        #This will work as a function for sending information to the Raspberry PI.
-        cv2.putText(frame, "Item Given!",(1170,100),0, 1,(255, 0, 0), 2)
 
 
     #Drawing "UI"
