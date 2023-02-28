24ca08e0eb5e4158a9274a7e29b9548925d91bbd
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-02-28 15:10:21 +0100

    Create Cmmands.py

---
+++ Voice Reg/Cmmands.py
@@ -0,0 +1,12 @@
+from subprocess import call
+
+def Check_voice():
+    call(["python","Voice Reg\Voice Reg.py"])
+    
+
+while True:
+    Check_voice()
+    if text == "1":
+        print("damn its 1")
+    else:
+        print("nope no 1")
