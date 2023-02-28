236d5f599dd7c9df17845c996751811bd8b84166
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-02-28 14:56:21 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,19 @@
+#first we import the nessecary libraries
 from subprocess import call
 import time as t
 import sys
+
+#here we create a function that allow us to call another script from this script
 def Open_color():
     call(["python","ColorRecognition\ColorRecognition.py"])
 
+#we then make a while true loop for checking the input in the console
 while True:
+    #we create a valuble called input_var that alow us writes an input in the console 
     input_var = input("Please enter something: ")
     
+    #here we check if input_var == run and if it is it then waits for 2 secounds then run the script function and then breaks out of the if statement
+    #if the input_var is not == run that it just prints "you entered" + what is writen in input_var  
     if input_var == "run":
         t.sleep(2)
         Open_color()
